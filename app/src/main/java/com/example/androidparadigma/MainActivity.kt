dab99edf9bc6dbc10c55e9f35c8eeb7888bbd929
enrique <luis.aguilar@ideaworks.la>
2020-07-16 11:13:26 -0600

    se agrega splashScreen, bottom navigation y iconos

--- app/src/main/java/com/example/androidparadigma/MainActivity.kt
+++ app/src/main/java/com/example/androidparadigma/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidparadigma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //open splash screen and time for splash screen
+        Thread.sleep(3000)
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
